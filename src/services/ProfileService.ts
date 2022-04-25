469f75dd5509fcce9f944703160a0e70dadb5a52
Davis Tibbz <33986524+Longwater1234@users.noreply.github.com>
2022-04-25 23:26:32 +0800

    course progress is complete. HURRRAY

--- src/services/ProfileService.ts
+++ src/services/ProfileService.ts
@@ -10,6 +10,11 @@ class ProfileService {
     getUserDetails() {
         return http.get("/profile/mine");
     }
+
+    /* enrolled courses with percent progress */
+    getShortProgress() {
+        return http.get("enroll/myprogress/top3");
+    }
 }
 
 export default new ProfileService();
