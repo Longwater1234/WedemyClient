15061c5ad0ef243a14475dfb649d76652e0b3537
Davis Tibbz <33986524+Longwater1234@users.noreply.github.com>
2022-07-12 15:41:28 +0800

    added path.join

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const serveStatic = require("serve-static");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(serveStatic(path.join(__dirname, "dist"), 
-{
-   maxAge: 7200000
-}
-));
+app.use(
+  serveStatic(path.join(__dirname, "dist"), {
+    maxAge: 7200000,
+  })
+);
+
+app.get(/.*/, function (req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(port);
 
