637b03681edb15c5d597ac03c4bbfb5936eabee5
Davis Tibbz <33986524+Longwater1234@users.noreply.github.com>
2022-07-12 15:21:05 +0800

    removed wildcard

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serveStatic = require("serve-static");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use("/", serveStatic(path.join(__dirname, "dist"), 
+app.use(serveStatic(path.join(__dirname, "dist"), 
 {
    maxAge: 7200000
 }
