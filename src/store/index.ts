f932eff281e376e542550f1ce12c125361827b35
Davis Tibbz <davisinyo@gmail.com>
2023-04-29 21:52:43 +0800

    read fullname only in status_login

--- src/store/index.ts
+++ src/store/index.ts
@@ -26,7 +26,7 @@ const myActions = {
         try {
             const res = await http.get("/auth/statuslogin");
             User.loggedIn = res.data.loggedIn;
-            User.username = res.data.user?.fullname || res.data.user;
+            User.username = res.data.fullname || "";
             return Boolean(User.loggedIn);
         } catch (error: any) {
             console.error(error.message);
